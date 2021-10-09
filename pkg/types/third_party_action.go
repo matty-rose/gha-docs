dd46e87e2c4db721a12c799f8a27e2a2d0bbebdb
Matthew Rose <matthew.rose@maxkelsen.com>
2021-10-09 15:26:37 +1000

    fix: correct package name

--- pkg/types/third_party_action.go
+++ pkg/types/third_party_action.go
@@ -19,7 +19,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
-package action
+package types
 
 // ThirdPartyAction represents a single third party action that is used within the composite action.
 type ThirdPartyAction struct {
