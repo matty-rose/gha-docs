218ca7c051d8fb4560bf1c2abd702066d97508f4
Matthew Rose <matthew.rose@maxkelsen.com>
2021-10-09 15:26:37 +1000

    fix: correct package name

--- pkg/types/output.go
+++ pkg/types/output.go
@@ -19,7 +19,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
-package action
+package types
 
 // Output represents a single output of a composite action.
 type Output struct {
